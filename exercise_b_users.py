cc6fa712341c3ce24918baef1b85b6504eac09a3
Jacob <jacob_greenhalgh@hotmail.com>
2022-06-21 21:38:48 +0100

    returns a list of Avrils even lottery numbers

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -65,6 +65,9 @@ print(users["Avril"]["pets"][0]["species"])
 # 5. Get the smallest of Erik's lottery numbers
 print(min(users["Erik"]["lottery_numbers"]))
 # 6. Return an list of Avril's lottery numbers that are even
+for num in users["Avril"]["lottery_numbers"]:
+    if num % 2 == 0:
+        print(num)
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 # 8. Change Erik's hometown to Edinburgh
 # 9. Add a pet dog to Erik called "fluffy"
