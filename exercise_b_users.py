d345d8d13c114470a227bed053423eef35c909ed
Jacob <jacob_greenhalgh@hotmail.com>
2022-06-21 22:06:34 +0100

    added Erik's pet dog fluffy

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -69,6 +69,11 @@ for num in users["Avril"]["lottery_numbers"]:
     if num % 2 == 0:
         print(num)
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+users["Erik"]["lottery_numbers"].append(7)
 # 8. Change Erik's hometown to Edinburgh
+users["Erik"]["home_town"]="Edinburgh"
+
 # 9. Add a pet dog to Erik called "fluffy"
+users["Erik"]["pets"][0] = {"name" : "fluffy", "species": "dog"}
 # 10. Add another person to the users dictionary
+users["Jim"]
