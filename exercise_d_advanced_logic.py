308241d8cce9fe0b504b7709bc56e734f92d727f
Jacob <jacob_greenhalgh@hotmail.com>
2022-06-21 23:55:53 +0100

    completed final task

--- exercise_d_advanced_logic.py
+++ exercise_d_advanced_logic.py
@@ -11,12 +11,27 @@ for num in numbers:
 print(max(numbers)-min(numbers))
 
 # 3. Print True if the list contains a 2 next to a 2 somewhere.
-
+i = 0
+for num in numbers:
+    if num == 2 and numbers[i+1] == 2:
+        print('true')
+    i = i + 1
 
 # 4. Print the sum of the numbers, 
 #    BUT ignore any section of numbers starting with a 6 and extending to the next 7.
 #    
 #    So [11, 6, 4, 99, 7, 11] would have sum of 22
+
+total = 0
+count = True
+for x in numbers:
+  if x == 6:
+    count = False
+  if count:
+    total += x
+  if x == 7:
+    count = True
+print (total)
 
 
 # 5. HARD! Print the sum of the numbers. 
@@ -26,6 +41,15 @@ print(max(numbers)-min(numbers))
 #
 #    So [5, 13, 2] would have sum of 5. 
 
+total = 0
+off_count=2
+for y in numbers:
+    if y == 13:
+        off_count=0
+    if off_count >= 2:
+        total+=y
+    off_count+=1
+print (total)
 
 
 
